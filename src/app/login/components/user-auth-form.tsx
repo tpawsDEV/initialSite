e49d4181a37f9252f48259f5283edd31a7184143
Yuri Martins <yuri@trempay.com>
2023-10-27 17:09:31 -0300

    update login

--- src/app/login/components/user-auth-form.tsx
+++ src/app/login/components/user-auth-form.tsx
@@ -7,10 +7,37 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { FaSpinner } from 'react-icons/fa'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const createUserFormSchema = z.object({
+  email: z.string()
+    .nonempty('O e-mail é obrigatório')
+    .email('Formato de e-mail inválido'),
+  password: z.string()
+  .nonempty('A senha é obrigatória')
+  .min(4, 'A senha precisa de no mínimo 4 caracteres')
+  .max(100)
+})
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+
+  const { register, handleSubmit, formState: { errors } } = useForm(
+    {
+      resolver: zodResolver(createUserFormSchema)
+    }
+  );
+
+  function createUser(data: any) {
+    console.log(data);
+  }
+
+
+
   const [isLoading, setIsLoading] = React.useState<boolean>(false)
 
   async function onSubmit(event: React.SyntheticEvent) {
@@ -24,30 +51,50 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit(createUser)}>
         <div className="grid gap-2">
           <div className="grid gap-1">
+            <h1 className="text-gray-400 text-base">Email</h1>
             <Label className="sr-only" htmlFor="email">
               Email
             </Label>
             <Input
-              id="email"
               placeholder="name@example.com"
               type="email"
               autoCapitalize="none"
-              autoComplete="email"
+              //autoComplete="email"
               autoCorrect="off"
               disabled={isLoading}
+              {...register('email')}
             />
+            {errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
           </div>
-          <Button disabled={isLoading}>
-            {isLoading && (
-              <FaSpinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Sign In with Email
-          </Button>
+          <div className="grid gap-1 ">
+            <h1 className="text-gray-400 text-base">Senha</h1>
+            <Label className="sr-only" htmlFor="senha">
+              senha
+            </Label>
+            <Input
+              placeholder="*******"
+              type="password"
+              // autoCapitalize="none"
+              // autoComplete="password"
+              // autoCorrect="off"
+              disabled={isLoading}
+              {...register('password')}
+            />
+            {errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
+          </div>
         </div>
-      </form>
-    </div>
+        <Button disabled={isLoading} className='w-full mt-5'>
+          {isLoading && (
+            <FaSpinner className="mr-2 h-4 w-4 animate-spin" />
+          )}
+          Login
+        </Button>
+
+      </form >
+    </div >
   )
 }
+
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,35 +16,30 @@ export default function AuthenticationPage() {
     <>
       <div className="container relative h-screen flex-col items-center justify-center grid grid-cols-1 lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-foreground dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-background" />
+          <div className="absolute inset-0 bg-black" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
+            <h1 className="text-white text-5xl">BoredWhy</h1>
           </div>
+          <div className="flex z-20 items-center justify-center h-full">
+            <img src="https://i.ibb.co/R0GQkq0/logobroed.png" className="w-96 h-96" />
+          </div>
+
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
-              <p className="text-lg">
+              <p className="text-lg text-white">
                 &ldquo;This library has saved me countless hours of work and
                 helped me deliver stunning designs to my clients faster than
                 ever before.&rdquo;
               </p>
-              <footer className="text-sm text-foreground">Sofia Davis</footer>
+              <footer className="text-sm text-foreground text-white">Sofia Davis</footer>
             </blockquote>
           </div>
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex justify-center items-center">
+              <img src="https://i.ibb.co/R0GQkq0/logobroed.png" className="w-12 h-12" />
+            </div>
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Acesse sua conta
